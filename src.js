989416fbe0d1e0d42450e561e5dd02b267fa5144
hashiq <mohdhashique10@gmail.com>
2022-10-05 22:59:39 +0530

    first commit

---
+++ src.js
@@ -0,0 +1,39 @@
+let expenses = [];
+const bars = document.querySelectorAll(".bar");
+const totalTag = document.querySelector(".left > h1");
+
+async function getExpenses() {
+    let data = await (await fetch("./data.json")).json();
+    expenses = data.map((expense) => {
+        return {
+            day: expense.day,
+            amount: expense.amount,
+        };
+    });
+}
+
+async function setChart() {
+    await getExpenses();
+    const maxExpense = findMaxAmount(expenses);
+    bars.forEach((bar, index) => {
+        const day = bar.querySelector(".day");
+        const percentage = expenses[index].amount / maxExpense;
+        day.textContent = expenses[index].day;
+        bar.style.height = `${percentage * 10}rem`; // 10rem
+    });
+    const totalExpense = expenses.reduce(
+        (total, expense) => total + expense.amount,
+        0
+    );
+    totalTag.textContent = "$" + Math.round(totalExpense);
+}
+
+function findMaxAmount(arr) {
+    let max = 0;
+    arr.forEach((expense) => {
+        if (expense.amount > max) max = expense.amount;
+    });
+    return max;
+}
+
+setTimeout(setChart, 500);
